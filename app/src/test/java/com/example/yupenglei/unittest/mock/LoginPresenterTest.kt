1d37f185cea028bae5edcf7a5d421f2431f0c187
penglei <yupengniaosanshi@gmail.com>
2017-09-28 17:55:40 +0800

    指定mock对象的函数行为；将mockito换为kotlin版本

--- app/src/test/java/com/example/yupenglei/unittest/mock/LoginPresenterTest.kt
+++ app/src/test/java/com/example/yupenglei/unittest/mock/LoginPresenterTest.kt
@@ -1,5 +1,6 @@
 package com.example.yupenglei.unittest.mock
 
+import com.nhaarman.mockito_kotlin.*
 import org.junit.Test
 import org.mockito.Mockito
 
@@ -13,48 +14,53 @@ class LoginPresenterTest {
 
     @Test
     fun login() {
-        val userManagerMocked = Mockito.mock(UserManager::class.java) // mock 对象
+//        val userManagerMocked = Mockito.mock(UserManager::class.java) // mock 对象
+
+        val userManagerMocked: UserManager = mock() // mock 对象
         val loginPresenter = LoginPresenter()
         loginPresenter.userManager = userManagerMocked
 
         loginPresenter.login(username, password) //执行
 
-        Mockito.verify(userManagerMocked).performLogin(username, password) // 验证
+        verify(userManagerMocked).performLogin(username, password) // 验证
     }
 
     @Test fun loginWithValidate() {
         val rightPwd = "tom is handsome"
         val wrongPwd = "tom is not handsome"
-        val mockValidator = Mockito.mock(PasswordValidator::class.java)
+        val mockValidator: PasswordValidator = mock {
+            on { verifyPassword(rightPwd) }.thenReturn(true)
+            on { verifyPassword(wrongPwd) }.thenReturn(false)
+        }
         //指定mock对象的函数返回值
-        Mockito.`when`(mockValidator.verifyPassword(rightPwd)).thenReturn(true)
-        Mockito.`when`(mockValidator.verifyPassword(wrongPwd)).thenReturn(false)
+//        Mockito.`when`(mockValidator.verifyPassword(rightPwd)).thenReturn(true)
+//        Mockito.`when`(mockValidator.verifyPassword(wrongPwd)).thenReturn(false)
 //        Mockito.`when`(mockValidator.verifyPassword(Mockito.anyString())).thenReturn(true)
 
-        val mockUserManager = Mockito.mock(UserManager::class.java)
+        val mockUserManager: UserManager = mock()
 
         val pwd = rightPwd
         LoginPresenter().apply { userManager = mockUserManager }
                 .apply { passwordValidator = mockValidator }
                 .loginWithValidate(username, pwd)
 
-        Mockito.verify(mockUserManager).performLogin(username, pwd)
+        verify(mockUserManager).performLogin(username, pwd)
     }
 
     @Test fun loginCallbackVersion() {
-        val mockValidator = Mockito.mock(PasswordValidator::class.java)
-        Mockito.`when`(mockValidator.verifyPassword(Mockito.anyString())).thenReturn(true)
-        val mockUserManager = Mockito.mock(UserManager::class.java)
-        Mockito.doAnswer {
-            val callback = it.arguments[2] as UserManager.NetWorkCallback
-            callback.onFailed(500, "server error")
-            return@doAnswer 22
-        }.`when`(mockUserManager.performLoginCallbackVersion(Mockito.anyString(),
-                Mockito.anyString(), Mockito.any(UserManager.NetWorkCallback::class.java)
-        ))
-
-        val loginPresenter = LoginPresenter()
-        loginPresenter.loginCallbackVersion(username, password)
+        val mockValidator: PasswordValidator = mock {
+            //注意whenever和on的区别
+            whenever(it.verifyPassword(any())).thenReturn(true)
+        }
+        val mockUserManager: UserManager = mock {
+            //指定mock对象函数行为
+            on { performLoginCallbackVersion(any(), any(), any()) }.doAnswer {
+                val callback = it.arguments[2] as UserManager.NetWorkCallback
+                callback.onFailed(500, "server error")
+            }
+        }
+        LoginPresenter().apply { passwordValidator = mockValidator }
+                .apply { userManager = mockUserManager }
+                .loginCallbackVersion(username, password)
     }
-
 }
